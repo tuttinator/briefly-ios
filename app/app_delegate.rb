317c3f06135009f001271ff213bbc1580043aa88
Caleb Tutty <caleb@prettymint.co.nz>
2014-06-01 10:41:36 +1200

    Navigation bar and tint colours

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,8 +4,16 @@ class AppDelegate
                          andNotificationTypes:(UIRemoteNotificationTypeBadge|
                                                UIRemoteNotificationTypeSound|
                                                UIRemoteNotificationTypeAlert))
+    UINavigationBar.appearance.tap do |o|
+      o.barTintColor = BubbleWrap.rgba_color(98, 95, 89, 1.0)
+      o.tintColor = BubbleWrap.rgb_color(78, 31, 22)
+
+      o.setTitleTextAttributes({
+        UITextAttributeTextColor => BubbleWrap.rgb_color(78, 31, 22)
+      })
+    end
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = ListViewController.new
+    @window.rootViewController = UINavigationController.alloc.initWithRootViewController(ListViewController.new)
     @window.makeKeyAndVisible
     true
   end
