c281367cb98b8d54f714a3175e1b609fb0a2b17d
Caleb Tutty <caleb@prettymint.co.nz>
2014-06-20 23:30:48 +1200

    Fixed whitespace

--- app/testflight_launcher.rb
+++ app/testflight_launcher.rb
@@ -2,7 +2,7 @@
 
 if Object.const_defined?('TestFlight') and !UIDevice.currentDevice.model.include?('Simulator')
   NSNotificationCenter.defaultCenter.addObserverForName(UIApplicationDidFinishLaunchingNotification, object:nil, queue:nil, usingBlock:lambda do |notification|
-  
+
   TestFlight.takeOff('5949c331-e0e8-433a-9be4-d20eeaf4af68')
   end)
 end
